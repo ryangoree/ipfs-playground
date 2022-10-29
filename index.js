875459b380b976cb6b9a218456c622242d33504f
Ryan Goree <goree.ryan@gmail.com>
2022-10-29 13:59:17 -0500

    Test file persistence

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ import toBuffer from 'it-to-buffer'
 
 const utf8decoder = new TextDecoder()
 
+async function write(node) {
+  return node.files.write('/stuff/hello.txt', 'Hello, World!!', {
+    create: true,
+    parents: true,
+  })
+}
+
+async function read(node) {
+  let secretMessage = await toBuffer(node.files.read('/stuff/hello.txt'))
+  console.log(utf8decoder.decode(secretMessage))
+}
+
 async function main() {
   const node = await IPFS.create()
-  let secretMessage = await toBuffer(node.files.read('/stuff/mfs.txt'))
-  console.log(utf8decoder.decode(secretMessage))
+  try {
+    await read(node)
+  } catch(err) {
+    console.error(err);
+    await write(node)
+    await read(node)
+  }
 }
 
 main()
